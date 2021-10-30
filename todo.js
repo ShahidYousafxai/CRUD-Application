3f2733027e5f9bc16e2b01d22a715d595fd587af
shahid yousafxai <shahidbahadur1998@gmail.com>
2021-10-30 12:01:40 -0700

    Updated

--- todo.js
+++ todo.js
@@ -1,50 +1,46 @@
-const forminput = document.getElementById("input");
-const addbtn = document.getElementById("add");
-const deletebtn = document.getElementById("delete");
-const todolst = document.getElementById("todo");
-const showbtn = document.getElementById("show")
+const forminput = document.getElementById('input')
+const addbtn = document.getElementById('add')
+const deletebtn = document.getElementById('delete')
+const todolst = document.getElementById('todo')
+const showbtn = document.getElementById('show')
 
+var todoList = []
 
-var todoList = [];
-
-class Todo
-{
-  todoval;
-  constructor(todoval) 
-  {
-    this.todoval = todoval;  
+class Todo {
+  todoval
+  constructor(todoval) {
+    this.todoval = todoval
   }
 }
 
-addbtn.addEventListener("click", function () 
-{
-  const todo = new Todo(forminput.value);
-  todoList.push(todo);
-  const todoJson = JSON.stringify(todoList);
-  localStorage.setItem("todoList", todoJson);
-});
+addbtn.addEventListener('click', function () {
+  if (forminput.value) {
+    const todo = new Todo(forminput.value)
+    todoList.push(todo)
+    const todoJson = JSON.stringify(todoList)
+    localStorage.setItem('todoList', todoJson)
+  } else {
+    alert('Please enter some text...!')
+  }
+})
 
-showbtn.addEventListener("click", function () 
-{
-  showUsers();
-});
+showbtn.addEventListener('click', function () {
+  showUsers()
+})
 
-
-function showUsers() 
-{
-  const userStorage = localStorage.getItem("todoList");
-  const JSONToUser = JSON.parse(userStorage);
-  for (let i = 0; i < JSONToUser.length; i++) 
-  {
-    const listItem = document.createElement("li");
+function showUsers() {
+  const userStorage = localStorage.getItem('todoList')
+  const JSONToUser = JSON.parse(userStorage)
+  for (let i = 0; i < JSONToUser.length; i++) {
+    const listItem = document.createElement('li')
     // liItem.textContent = JSONToUser[i].name + " ( " + JSONToUser[i].email + " )";
     listItem.textContent = JSONToUser[i].todoval
-    todolst.appendChild(listItem);
+    todolst.appendChild(listItem)
   }
 }
 
-deletebtn.addEventListener("click", function () {
-  todoList = todoList.filter((todo) => todo.todoval != forminput.value);
-  const usersJson = JSON.stringify(users);
-  localStorage.setItem("users", usersJson);
-});
+deletebtn.addEventListener('click', function () {
+  todoList = todoList.filter((todo) => todo.todoval != forminput.value)
+  const usersJson = JSON.stringify(users)
+  localStorage.setItem('users', usersJson)
+})
